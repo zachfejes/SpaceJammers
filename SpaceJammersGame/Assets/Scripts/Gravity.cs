77f2a7fac76493ada4f6bfa68268dbbfff9232e4
Zach <zacharyfejes@gmail.com>
2015-10-03 13:36:01 -0400

    Added Gravity to Asteroids
    Now have proper Newtonian (2-body) gravity in the game for each
    asteroid. Also created a gameController object and script that as-yet
    don't do anything.

--- SpaceJammersGame/Assets/Scripts/Gravity.cs
+++ SpaceJammersGame/Assets/Scripts/Gravity.cs
@@ -3,23 +3,34 @@ using System.Collections;
 
 public class Gravity : MonoBehaviour {
 
-	public float targetGrav;
-	public float asteroidGrav;
+	public float targetMass = 5.972e24f;
+	public float asteroidMass = 4e10f;
     public GameObject target;
-    public float pullForce;
+    public GameObject control;
+    public float gravForce;
+    float G = 6.674e-11f; // N*(m/kg)^2
+    float universeScale = 40000000/10;
+    public float timeScale = 1;
+    public float initialKick = 0;
 
 	// Use this for initialization
 	void Start () {
 	    // Create a random initial velocity (also an apoapsis)
-
+        Vector2 forceDirection = Vector2.up;
+        transform.GetComponent<Rigidbody2D>().AddForce(forceDirection * initialKick * (Time.fixedDeltaTime));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //calculate direction to target
-		Vector2 forceDirection = (Vector2)(target.transform.position - transform.position);
+        //calculate direction to target (not yet normalized)
+		Vector2 forceDirection = universeScale*(Vector2)(target.transform.position - transform.position);
 
-        //apply gravetic acceleration
-        transform.GetComponent<Rigidbody2D>().AddForce(forceDirection.normalized*pullForce*Time.fixedDeltaTime);
+        //calculate scalar force component
+        gravForce = G * targetMass * asteroidMass / Vector2.SqrMagnitude(forceDirection); //////// Working on this.
+
+        //apply gravetic acceleration (vector)
+        transform.GetComponent<Rigidbody2D>().AddForce(forceDirection.normalized*gravForce*(timeScale*Time.fixedDeltaTime));
+        //Debug.Log(Time.fixedDeltaTime);
+
 	}
 }
---
+++ SpaceJammersGame/Assets/Scripts/gameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+
+    // The scale factor we will be using (just as a standard) will be that 10 distance units = 40000km = 40000000m (appx. Geo-orbit). This assumes that we are working in [m].
+    static float universeScale = 1/40000000; //[m]
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
